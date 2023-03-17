d38f7e32a4d8f4e212c42847153731989e0c3ff1
Mauricio Chaile <xaviermauricio33@gmail.com>
2023-03-17 12:27:18 -0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const el = document.querySelector("#root");
+
+const root = ReactDOM.createRoot(el);
+
+function App(){
+    let message = 'bye there';
+
+    if (Math.random()>0.5) {
+        message= 'Hello there!'
+    }
+    return <h1>{message}</h1>
+}
+
+root.render(<App/>)
